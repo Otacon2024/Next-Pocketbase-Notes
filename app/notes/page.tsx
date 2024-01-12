8fcc3224f214a9ef3529f7701b55114a91cceaef
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2024-01-12 23:55:02 +0100

    Add getNotes function to fetch and return data from the PocketBase API

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,4 +1,15 @@
-async function getNotes() {}
+async function getNotes() {
+  // fetch data from a pocketbase url endpoint
+  const res = await fetch(
+    "http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30"
+  );
+
+  // convert to json
+  const data = await res.json();
+
+  // return the data as an array
+  return data?.items as any[];
+}
 
 export default async function NotesPage() {
   return (
